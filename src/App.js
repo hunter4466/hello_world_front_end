b5d17f16c9b11f2b68a417c3846bfe5e40ceef22
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-30 18:36:53 -0500

    Index: Added index entry point and app files to display information from redux store

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-  Redirect,
-} from 'react-router-dom';
-import HomePage from './components/homepage';
-import ShoppingCart from './components/shoppingcart';
-import Store from './components/store';
-import { resetCartSwitch } from './redux/cart/cart';
-import { resetStore } from './redux/store/store';
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchMessages } from "./redux/app/app"
 
 const App = () => {
-  const dispatch = useDispatch();
-  const handleStoreCloseClick = () => {
-    dispatch(resetStore());
-    dispatch(resetCartSwitch());
-  };
-  return (
-    <Router>
-      <div className="page_holder">
-        <div className="navigator_bar">
-          <NavLink activeClassName="selected_nav_item" className="nav_item" onClick={() => { handleStoreCloseClick(); }} to="/home">HOME</NavLink>
-          <NavLink activeClassName="selected_nav_item" className="nav_item" onClick={() => { handleStoreCloseClick(); }} to="/store">TIENDA</NavLink>
-          <NavLink activeClassName="selected_nav_item" className="nav_item" onClick={() => { handleStoreCloseClick(); }} to="/shop">CARRITO</NavLink>
-        </div>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
-          <Route path="/home">
-            <HomePage />
-          </Route>
-          <Route path="/store">
-            <Store />
-          </Route>
-          <Route path="/shop">
-            <ShoppingCart />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+    const state = useSelector((state) => state.messagesReducer)
+    const dispatch = useDispatch()
+    useEffect(()=> {
+       dispatch(fetchMessages())
+    }, [])
+    return (
+        <div>Greetings: {state.message}</div>
+    )
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import store from './redux/configureStore';
-import './styles/_fonts.scss';
-import './styles/_store.scss';
-import './styles/_main.scss';
-import './styles/_cart.scss';
-import './styles/_home.scss';
 
 ReactDOM.render(
   <React.StrictMode>
