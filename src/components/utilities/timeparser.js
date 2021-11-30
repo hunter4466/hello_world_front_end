a1a85baf7fb5dc42290c46703b57bf6a90443382
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-30 18:40:12 -0500

    Redux: Added proprer configuration to reduxx store in order to fetch the information from API

--- src/components/utilities/timeparser.js
+++
@@ -1,3 +0,0 @@
-const msToTime = (ms) => new Date(ms).toISOString().slice(11, -5);
-
-export default msToTime;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,64 +1,24 @@
 import {
   createStore, compose, combineReducers, applyMiddleware,
 } from 'redux';
-import logger from 'redux-logger';
 // ----------- STORE IMPORTS -----------
 import {
-  // --- Reducers --
-  storeSwitchReducer,
-  storemainReducer,
-  storeSecondaryReducer,
-  storePickerReducer,
-  itemPickerReducer,
-  itemBuildReducer,
-  productBuildReducer,
-  // --- Middlewares --
-  getCategoriesFromAPIMiddleware,
-  getProductsFromAPIMiddleware,
-  getItemsFromAPIMiddleware,
-  addItemToProductMiddleware,
-  addFinalProductHeaderMiddleware,
-  loadingScreensMiddleware,
-} from './store/store';
-// ----------- CART IMPORTS -----------
-import {
-  // --- Reducers --
-  cartReducer,
-  cartSwitchReducer,
-  cartStorePickerReducer,
-  cartItemPickerReducer,
-  // --- Middlewares --
-  saveCartInfoMiddleware,
-} from './cart/cart';
+ // -------------- reducers ---------------
+ messagesReducer,
+ // -------------- actions ----------------
+ // ------------- middlewares -------------
+ fetchMessagesFromAPIMiddleware,
+} from './app/app';
 
 const reducer = combineReducers({
   // ------------ Store Reducers -----
-  storeSwitchReducer,
-  storemainReducer,
-  storeSecondaryReducer,
-  storePickerReducer,
-  itemPickerReducer,
-  itemBuildReducer,
-  productBuildReducer,
-  // ----------- Cart Reducers ------
-  cartReducer,
-  cartSwitchReducer,
-  cartStorePickerReducer,
-  cartItemPickerReducer,
+  messagesReducer,
 });
 
 const composedEnhancer = compose(
   // ------------ Store Middlewares -----
-  applyMiddleware(getCategoriesFromAPIMiddleware),
-  applyMiddleware(getProductsFromAPIMiddleware),
-  applyMiddleware(getItemsFromAPIMiddleware),
-  applyMiddleware(addItemToProductMiddleware),
-  applyMiddleware(addFinalProductHeaderMiddleware),
-  applyMiddleware(loadingScreensMiddleware),
-  // ------------ Cart Middlewares -----
-  applyMiddleware(saveCartInfoMiddleware),
+  applyMiddleware(fetchMessagesFromAPIMiddleware),
   // ------------- Logger --------------
-  applyMiddleware(logger),
 );
 
 const store = createStore(
